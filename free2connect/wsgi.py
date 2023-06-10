15ed6651d5c56ba100e99304228a398ae978db95
Sumit <sumitgirwals@gmail.com>
2023-06-10 15:04:20 +0530

    Created basic project and app

---
+++ free2connect/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for free2connect project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'free2connect.settings')
+
+application = get_wsgi_application()
