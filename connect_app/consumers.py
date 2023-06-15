b8a1c1ac72cd7713bd2062ba56b83b88934d3fd0
Sumit <sumitgirwals@gmail.com>
2023-06-15 14:04:19 +0530

    Added the chat bidirection and chat template

--- connect_app/consumers.py
+++ connect_app/consumers.py
@@ -157,43 +157,48 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
 
         print(self.room_name)
-        # self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'chat_%s' % self.room_name
 
         # # Join room group
-        # await self.channel_layer.group_add(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
 
         await self.accept()
 
     async def disconnect(self, close_code):
         # Leave room group
-        # await self.channel_layer.group_discard(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
-        pass
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+         
 
-    async def receive(self, text_data):
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json['message']
+    async def receive(self, text_data): 
+        text_data_json = json.loads(text_data)
+        user_id = text_data_json['user_id']
+        message = text_data_json['message']
 
-        # # Send message to room group
-        # await self.channel_layer.group_send(
-        #     self.room_group_name,
-        #     {
-        #         'type': 'chat_message',
-        #         'message': message
-        #     }
-        # )
-        pass
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'user_id': user_id,
+                'message': message
+            }
+        )
+        
 
     async def chat_message(self, event):
-        # message = event['message']
+        user_id = event['user_id']
+        message = event['message']
 
-        # # Send message to WebSocket
-        # await self.send(text_data=json.dumps({
-        #     'message': message
-        # }))
-        pass
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'user_id': user_id,
+            'message': message
+
+        }))
+        
